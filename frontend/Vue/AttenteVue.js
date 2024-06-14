98334bcce54d90d61f8a0e04277a645a816885b0
nicopyright <nico.f3ry@gmail.com>
2024-06-14 20:33:14 +0200

    Mise en place SSE

--- frontend/Vue/AttenteVue.js
+++ frontend/Vue/AttenteVue.js
@@ -1,11 +1,29 @@
 import VueAZero from "./VideVue.js";
 import {RejoindreVue} from "./RejoindreVue.js";
+import {SSEClient} from "../libs/sse-client.js";
 
 export class AttenteVue {
+    #clientSSE
     constructor() {
+        this.#clientSSE = new SSEClient("http://localhost:8080");
     }
     afficherAttente() {
         VueAZero();
+        let jeton = sessionStorage.getItem("jeton");
+        this.#clientSSE.connect();
+        this.#clientSSE.subscribe("attente", (reponse) => {
+            let donnee = JSON.parse(reponse);
+            let role = donnee.role;
+            let grille = donnee.grille;
+            if (role === 1) {
+                let maitreMotVue = new MaitreMotVue();
+                maitreMotVue.afficherMaitreMot(grille);
+            }
+            else if (role === 2) {
+                let maitreIntuitionVue = new MaitreIntuitionVue();
+                maitreIntuitionVue.afficherMaitreIntuition(grille);
+            }
+        });
         fetch("Vue/Patron/attente.html").then(response => response.text()).then(text => {
             document.getElementById("principal").innerHTML += text;
             let boutonMenu = document.getElementsByTagName("button")[0];
