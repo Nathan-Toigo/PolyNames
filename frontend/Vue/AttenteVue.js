3a8a40620d988b65d4411c5ee5573291dc763820
nicopyright <nico.f3ry@gmail.com>
2024-06-14 21:30:07 +0200

    fix subscribe to canal_distant

--- frontend/Vue/AttenteVue.js
+++ frontend/Vue/AttenteVue.js
@@ -5,13 +5,26 @@ import {SSEClient} from "../libs/sse-client.js";
 export class AttenteVue {
     #clientSSE
     constructor() {
-        this.#clientSSE = new SSEClient("http://localhost:8080");
+        this.#clientSSE = new SSEClient("localhost:8080");
     }
     async afficherAttente() {
         VueAZero();
         let jeton = sessionStorage.getItem("jeton");
+        let canal_distant_jeton = "canal_distant_".concat(jeton);
         await this.#clientSSE.connect();
-        await this.#clientSSE.subscribe("canal_distant_".concat(Jeton), (reponse) => {
+
+        fetch("Vue/Patron/attente.html").then(response => response.text()).then(text => {
+            document.getElementById("principal").innerHTML += text;
+            let boutonMenu = document.getElementsByTagName("button")[0];
+            boutonMenu.addEventListener("click", async () => {
+                let rejoindreVue = new RejoindreVue();
+                rejoindreVue.afficherRejoindre();
+                this.#clientSSE.unsubscribe(canal_distant_jeton);
+
+            });
+        });
+
+        await this.#clientSSE.subscribe(canal_distant_jeton, (reponse) => {
             let donnee = JSON.parse(reponse);
             let role = donnee.role;
             let grille = donnee.grille;
@@ -23,15 +36,6 @@ export class AttenteVue {
                 let maitreIntuitionVue = new MaitreIntuitionVue();
                 maitreIntuitionVue.afficherMaitreIntuition(grille);
             }
-        });
-        fetch("Vue/Patron/attente.html").then(response => response.text()).then(text => {
-            document.getElementById("principal").innerHTML += text;
-            let boutonMenu = document.getElementsByTagName("button")[0];
-            boutonMenu.addEventListener("click", async () => {
-                let rejoindreVue = new RejoindreVue();
-                rejoindreVue.afficherRejoindre();
-
-            });
         });
     }
 }
